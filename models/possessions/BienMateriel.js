3eb5e5ab8790b19c4535a7e801198adef5e7b502
NESWILLY <hei.neswilly.2@gmail.com>
2024-09-08 10:51:01 +0300

    feat: add type handling to BienMateriel class and improve error handling

--- models/possessions/BienMateriel.js
+++ models/possessions/BienMateriel.js
@@ -1,7 +1,14 @@
 import Possession from "./Possession.js";
 export default class BienMateriel extends Possession {
-  constructor(possesseur, libelle, valeur, dateDebut, dateFin, tauxAmortissement) {
+  constructor(possesseur, libelle, valeur, dateDebut, dateFin, tauxAmortissement, type) {
     super(possesseur, libelle, valeur, dateDebut, dateFin, tauxAmortissement);
+    
+    try {
+      this.type = type;
+    }
+    catch (e) {
+      console.error(e);
+    }
   }
 
   getValeur(date) {
